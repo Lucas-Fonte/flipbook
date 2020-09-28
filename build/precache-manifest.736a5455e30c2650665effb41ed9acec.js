9ee58a54439051cc453c284a0221b23937ff9790
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-09-28 17:41:45 -0300

    first commit

---
+++ build/precache-manifest.736a5455e30c2650665effb41ed9acec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab486369c54edaec7ec6dbda41f027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60428538ef1dd1ce5e33",
+    "url": "/static/css/main.60bf7af8.chunk.css"
+  },
+  {
+    "revision": "bf819ed077ea568d5027",
+    "url": "/static/js/2.59832bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59832bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60428538ef1dd1ce5e33",
+    "url": "/static/js/main.b5ad20a8.chunk.js"
+  },
+  {
+    "revision": "872a4f38a4c1834ffb0d",
+    "url": "/static/js/runtime-main.208032d0.js"
+  }
+]);
