f622eb7860741d67ff215ed5f2d3303de6add21a
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-09-28 17:39:01 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HTMLFlipBook from 'react-pageflip';
+import './book.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="book">
+      <HTMLFlipBook
+        width={550}
+        height={733}
+        size="stretch"
+        minWidth={315}
+        maxWidth={1000}
+        minHeight={400}
+        maxHeight={1533}
+        maxShadowOpacity={0.5}
+        showCover={false}
+      >
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg"
+            alt="page1"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/1/14/Segovia_-_Acueducto_03.jpg"
+            alt="page2"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gyeongbokgung-GeunJeongJeon.jpg"
+            alt="page3"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Kinkaku3411.jpg"
+            alt="page4"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg"
+            alt="page5"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg"
+            alt="page6"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/1/14/Segovia_-_Acueducto_03.jpg"
+            alt="page7"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gyeongbokgung-GeunJeongJeon.jpg"
+            alt="page8"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Kinkaku3411.jpg"
+            alt="page9"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg"
+            alt="page10"
+          ></img>
+        </div>
+      </HTMLFlipBook>
     </div>
   );
 }
